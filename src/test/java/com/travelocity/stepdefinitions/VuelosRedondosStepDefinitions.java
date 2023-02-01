ad93d48456b6cc5f0d2c8e12499a9deb9d72fee9
Diego Roman <drjroman06@gamil.com>
2023-02-01 16:30:54 -0500

    Step definitions de vuelos implementados

--- src/test/java/com/travelocity/stepdefinitions/VuelosRedondosStepDefinitions.java
+++ src/test/java/com/travelocity/stepdefinitions/VuelosRedondosStepDefinitions.java
@@ -1,12 +1,15 @@
 package com.travelocity.stepdefinitions;
 
+import com.travelocity.tasks.SeleccionarClaseVuelo;
 import com.travelocity.tasks.SeleccionarDestinoVuelos;
 import com.travelocity.tasks.SeleccionarOrigenVuelos;
+import com.travelocity.userinterface.DetalleVuelos;
 import com.travelocity.userinterface.VuelosEncontrados;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.ensure.Ensure;
 
@@ -26,8 +29,62 @@ public class VuelosRedondosStepDefinitions {
         );
     }
 
-    @Entonces("debe obtener alguna opcion de vuelo")
+    @Entonces("debe obtener al menos una opcion de vuelo")
     public void viajeObtenerAlgunaOpcion() {
+        Ensure.that(VuelosEncontrados.LIST_VUELOS_ENCONTRADOS).values().hasSizeGreaterThan(0);
+    }
+
+    @Dado("{actor} esta en {string} y desea tomar un vuelo en {string}")
+    public void vueloEnClaseEconomicaPremium(Actor actor, String origen, String claseVuelo) {
+        actor.attemptsTo(
+                SeleccionarOrigenVuelos.para(origen),
+                SeleccionarClaseVuelo.en(claseVuelo)
+        );
+    }
+    @Cuando("realice el proceso para viajar a {string} por {int} dias")
+    public void viajeDestino(String destinoViaje, int diasViaje) {
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                SeleccionarDestinoVuelos.destinoYDias(destinoViaje, diasViaje)
+        );
+    }
+    @Entonces("debe obtener al menos una opcion de vuelo en clase economica premium")
+    public void opcionViajeEnClaseEconomica() {
+        Ensure.that(VuelosEncontrados.LIST_VUELOS_ENCONTRADOS).values().hasSizeGreaterThan(0);
+    }
+
+    @Dado("{actor} esta en {string} y desea viajar")
+    public void agregarAutoParaEstadia(Actor usuario, String origenUsuario) {
+        usuario.attemptsTo(
+                SeleccionarOrigenVuelos.para(origenUsuario)
+        );
+    }
+    @Cuando("realice el proceso para viajar a {string} por {int} dias con la opcion agregar un auto")
+    public void viajeOpcionAgregarAuto(String destinoViaje, Integer cantDias) {
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                Click.on(DetalleVuelos.INP_CHECK_VUELOS).afterWaitingUntilEnabled(),
+                SeleccionarDestinoVuelos.destinoYDias(destinoViaje, cantDias)
+        );
+    }
+    @Entonces("debe obtener al menos una opcion de vuelo con auto incluido")
+    public void obtenerOpcionVueloYAuto() {
+        Ensure.that(VuelosEncontrados.LIST_VUELOS_ENCONTRADOS).values().hasSizeGreaterThan(0);
+    }
+
+    @Dado("{actor} esta en {string} y desea viajar con hospedaje incluido")
+    public void viajeConHospedaje(Actor usuario, String origenUsuario) {
+        usuario.attemptsTo(
+                SeleccionarOrigenVuelos.para(origenUsuario)
+        );
+    }
+    @Cuando("realice el proceso para viajar a {string} por {int} dias con hospedaje por {int} dias")
+    public void realice_el_proceso_de_para_viajar_a_cancun_con_hospedaje_por_dias(String destinoVuelo, Integer cantDias, Integer cantDiasHospedaje) {
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                Click.on(DetalleVuelos.INP_CHECK_HOSPEDAJE).afterWaitingUntilEnabled(),
+                SeleccionarDestinoVuelos.destinoYDias(destinoVuelo, cantDias)
+        );
+    }
+    @Entonces("debe obtener al menos una opcion de vuelo con hospedaje incluido")
+    public void debe_obtener_al_menos_una_opcion_de_vuelo_con_hospedaje_incluido() {
         Ensure.that(VuelosEncontrados.LIST_VUELOS_ENCONTRADOS).values().hasSizeGreaterThan(0);
     }
 
