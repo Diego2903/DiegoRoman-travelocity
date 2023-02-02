cb0836fe0311ab7558cbffd3296e6750d2306290
Diego Roman <drjroman06@gamil.com>
2023-02-02 13:54:15 -0500

    correcciones generales

--- src/test/java/com/travelocity/userinterface/DetalleHospedaje.java
+++ src/test/java/com/travelocity/userinterface/DetalleHospedaje.java
@@ -10,7 +10,7 @@ public class DetalleHospedaje {
     public static final Target SELECCIONAR_CHECK_OUT = Target.the("selector de fecha salida").locatedByFirstMatching("//button[@data-day='16']");
     public static final Target BUT_LISTO = Target.the("boton listo fecha").locatedBy("//button[@data-stid='apply-date-picker']");
     public static final Target DIV_SELECCIONAR_NUMERO_PERSONAS = Target.the("boton seleccionar numero de personas").locatedBy("//div[@id='adaptive-menu']");
-    public static final Target INP_CANTIDAD_PERSONAS = Target.the("input cantidad personas").locatedBy("//input[@id='adult-input-0']");
+    public static final Target INP_CANTIDAD_PERSONAS = Target.the("input cantidad personas").locatedBy("//input[@id='adult-input-0']/following-sibling::button");
     public static final Target BUT_LISTO_NUMERO_PERSONAS = Target.the("boton listo numero personas").locatedBy("//button[@data-testid='guests-done-button']");
     public static final Target BUT_BUSCAR = Target.the("button buscar hospedaje").locatedBy("//button[@data-testid='submit-button']");
     public static final Target INP_CHECK_AUTO = Target.the("input seleccionar agregar auto").locatedBy("//input[@id='add-car-switch']");
--- src/test/java/com/travelocity/userinterface/DetalleVuelos.java
+++ src/test/java/com/travelocity/userinterface/DetalleVuelos.java
@@ -7,12 +7,14 @@ public class DetalleVuelos {
     public static final Target INP_ORIGEN = Target.the("input de origen").locatedBy("//input[@id='location-field-leg1-origin']");
     public static final Target BUT_DESTINO = Target.the("button de destino").locatedBy("//button[@data-stid='location-field-leg1-destination-menu-trigger']");
     public static final Target INP_DESTINO = Target.the("input de destino").locatedBy("//input[@id='location-field-leg1-destination']");
-    public static final Target SELECCIONAR_DATE_REGRESO = Target.the("selector de fecha salida").locatedBy("//button[@id='d2-btn']");
+    public static final Target BUT_ABRIR_FECHAS = Target.the("selector de fecha salida").locatedBy("//button[@id='d1-btn']");
     public static final Target DATE_REGRESO = Target.the("calendar fecha regreso").locatedByFirstMatching("//button[@data-day='17']");
     public static final Target BUT_LISTO = Target.the("boton listo fecha").locatedBy("//button[@data-stid='apply-date-picker']");
     public static final Target BUT_BUSCAR = Target.the("button buscar vuelos").locatedBy("//button[@data-testid='submit-button']");
     public static final Target INP_CHECK_VUELOS = Target.the("Input checkbox agregar auto").locatedBy("//input[@id='add-car-checkbox']");
     public static final Target INP_CHECK_HOSPEDAJE = Target.the("Input checkbox agregar hospedaje").locatedBy("//input[@id='add-hotel-checkbox']");
     public static final Target DIV_SELECCIONAR_CLASE_VUELO = Target.the("boton seleccionar clase vuelo").locatedBy("//div[@id='preferred-class-input']");
+    public static final Target BUT_SELECCIONAR_CHECK_IN = Target.the("buton selccionar check-in").locatedBy("//button[contains(@aria-label, 'Check-in')]");
+    public static final Target BUT_LISTO_CHECK_IN = Target.the("buton listo fechas hospedaje").locatedBy("//button[@data-stid='apply-date-picker']");
 
 }
